74168fc65d601e484e82d86af9570df1b0b3c64f
César Ravelo Martínez <crmprograming@gmail.com>
2020-10-14 16:05:43 +0100

    Cambiada la configuración de acceso a un fichero aparte

--- php/conexion.php
+++ php/conexion.php
@@ -1,12 +1,11 @@
 <?php
+	include("config.php");
 	function conectaDB() {
 		try {
-			$host = "localhost";
-			$dbname = "bdfutbol";
-			$user = "root";
-		   	$pass = "";
 			$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			unset($host, $dbname, $user, $pass);
 
 		} catch(PDOException $e) {
 			echo $e->getMessage();
