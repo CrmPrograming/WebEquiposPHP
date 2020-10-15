c49538d4c1e0993a6d290d8ca236ef03062e44fd
César Ravelo Martínez <crmprograming@gmail.com>
2020-10-15 17:05:53 +0100

    Ajustado error de diseño en la conexión a la bd

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,8 @@
 <?php
-	include("config.php");
 	function conectaDB() {
+		$_config = include("config.php");
 		try {
-			$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+			$pdo = new PDO("mysql:host=$_config[host];dbname=$_config[dbname];charset=utf8", $_config['user'], $_config['pass']);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 			unset($host, $dbname, $user, $pass);
