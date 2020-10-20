144a137b1aa7436bf90770c58c6613f8c3811678
César Ravelo Martínez <crmprograming@gmail.com>
2020-10-20 09:30:31 +0100

    Añadida documentación y corregido unset

--- php/conexion.php
+++ php/conexion.php
@@ -1,11 +1,13 @@
 <?php
+	// Función encargada de establecer conexión con la base de datos.
+	// Requiere del fichero config.php para funcionar correctamente.
 	function conectaDB() {
 		$_config = include("config.php");
 		try {
 			$pdo = new PDO("mysql:host=$_config[host];dbname=$_config[dbname];charset=utf8", $_config['user'], $_config['pass']);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-			unset($host, $dbname, $user, $pass);
+			unset($_config);
 
 		} catch(PDOException $e) {
 			echo $e->getMessage();
